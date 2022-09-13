a4f071e35fe51a9259089d195371fe235cbec571
Christian Magro <cmagrop@gmail.com>
2022-09-12 21:44:32 -0300

    Se agrego ProductRepository

---
+++ app/src/main/java/com/example/backlogapp/repository/ProductRepository.kt
@@ -0,0 +1,44 @@
+package com.example.backlogapp.repository
+
+import androidx.lifecycle.LiveData
+import com.example.backlogapp.model.ProductDAO
+import com.example.backlogapp.model.ProductModel
+
+class ProductRepository(val productDAO: ProductDAO) {
+
+    val leerTodosLosDatos:LiveData<List<ProductModel>> = productDAO.leerTodosLosDatos()
+
+    fun agregarProducto(product:ProductModel)
+    {
+        productDAO.insertarProducto(product)
+    }
+
+    fun actualizarProducto(product:ProductModel)
+    {
+        productDAO.actualizarProducto(product)
+
+    }
+
+    fun eliminarProducto(product:ProductModel)
+    {
+        productDAO.eliminarProducto(product)
+    }
+
+
+}
+
+
+/*
+
+class ProductRepository<ProductDAO productdao>{
+
+ ProductRepository(ProductDAO product)
+ {
+    this.productdao = product;
+ }
+
+}
+
+
+
+* */
